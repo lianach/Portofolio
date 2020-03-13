2bf0ef147732133801e3d7bd6740d07891ac6238
lianach <liana.andreea97@yhaoo.com>
2020-03-13 01:10:21 -0400

    bug

--- server/index.js
+++ server/index.js
@@ -17,29 +17,29 @@ const createApp = () => {
 
   app.use(express.static(path.join(__dirname, '..', 'public')))
 
-  app.use((req, res, next) => {
-    if (path.extname(req.path).length) {
-      const err = new Error('Not found')
-      err.status = 404
-      next(err)
-    } else {
-      next()
-    }
-  })
+  // app.use((req, res, next) => {
+  //   if (path.extname(req.path).length) {
+  //     const err = new Error('Not found')
+  //     err.status = 404
+  //     next(err)
+  //   } else {
+  //     next()
+  //   }
+  // })
 
   app.use('*', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'public/index.html'))
   })
 
-  app.use((err, req, res, next) => {
-    console.error(err)
-    console.error(err.stack)
-    res.status(err.status || 500).send(err.message || 'Internal server error.')
-  })
+  // app.use((err, req, res, next) => {
+  //   console.error(err)
+  //   console.error(err.stack)
+  //   res.status(err.status || 500).send(err.message || 'Internal server error.')
+  // })
 }
 
 const startListening = () => {
-  const server = app.listen(PORT, () =>
+  app.listen(PORT, () =>
     console.log(`Mixing it up on port ${PORT}`)
   )
 }
